0001a9ef44186874782d5c8dc3b2ac85edaa74a6
MohsinRiaz123 <164615053+MohsinRiaz123@users.noreply.github.com>
2024-07-07 04:57:13 +0500

    website
    html to react

---
+++ src/Body.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ProFeatue from './ProFeatue'
+import Hader from './Hader'
+import Slider from './Slider'
+import Schedule from './Schedule'
+import Feature from './Feature'
+import FunFact from './FunFact'
+import WhyChoose from './WhyChoose'
+import CallToAction from './CallToAction'
+import Portfolio from './Portfolio'
+import Services from './Services'
+import Pricingtable from './Pricingtable'
+import Blogs from './Blogs'
+import Clients from './Clients'
+import Appointment from './Appointment'
+import Newsletter from './Newsletter'
+import Footer from './Footer'
+function Body() {
+  return (
+    <>
+    <ProFeatue />
+      <Hader />
+      <Slider/>
+      <Schedule />
+      <Feature />
+      <FunFact />
+      <WhyChoose />
+      <CallToAction />
+      <Portfolio />
+      <Services />
+      <Pricingtable />
+      <Blogs />
+      <Clients />
+      <Appointment />
+      <Newsletter />
+      <Footer />
+  
+    </>
+  )
+}
+
+export default Body
